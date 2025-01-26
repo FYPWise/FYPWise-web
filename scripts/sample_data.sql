9c87e2f28b1d6ac75bfb759240e4e8ccb79e4da3
Nur Aisyah Nabila <ext_nuraisyahnabila.nahar@malaysiaairlines.com>
2025-01-26 18:57:14 +0800

    🔨  Add SQL scripts for database creation and sample data insertion

---
+++ scripts/sample_data.sql
@@ -0,0 +1,140 @@
+-- Select the database to work with
+USE fypwise;
+
+-- Set character encoding and collation to utf8mb4 for the connection
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `fypwise`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Insert sample data  for table `user`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Insert sample data  for table `lecturer`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Insert sample data  for table `student`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Insert sample data  for table `announcement`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Insert sample data  for table `task`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Insert sample data  for table `proposal`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Insert sample data  for table `proposal_status`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Insert sample data  for table `project`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Insert sample data  for table `project_timeline`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Insert sample data  for table `milestone`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Insert sample data  for table `timeline_file`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Insert sample data  for table `project_submission`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Insert sample data  for table `marksheet`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Insert sample data  for table `criteria_score`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Insert sample data  for table `meeting`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Insert sample data  for table `student_meeting`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Insert sample data  for table `meeting_log`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Insert sample data  for table `presentation`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Insert sample data  for table `message`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Insert sample data  for table `group`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Insert sample data  for table `user_group`
+--
+
+-- --------------------------------------------------------
---
+++ scripts/table_creation.sql
@@ -0,0 +1,143 @@
+-- Set character encoding and collation to utf8mb4 for the connection
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+-- Create a new database (optional)
+CREATE DATABASE IF NOT EXISTS fypwise;
+
+-- Select the database to work with
+USE fypwise;
+
+--
+-- Database: `fypwise`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lecturer`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `announcement`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `task`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `proposal`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `proposal_status`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `project`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `project_timeline`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `milestone`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `timeline_file`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `project_submission`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `marksheet`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `criteria_score`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `meeting`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student_meeting`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `meeting_log`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `presentation`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `message`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `group`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_group`
+--
+
+-- --------------------------------------------------------

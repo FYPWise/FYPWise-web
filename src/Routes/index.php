45f016af6c83c566978dd25b44cc5747d489577b
MohamedImran7868 <1211101935@student.mmu.edu.my>
2025-02-02 14:18:26 +0800

    ✨ Refactor project management routes into a separate file

--- src/Routes/index.php
+++ src/Routes/index.php
@@ -31,17 +31,6 @@ include "final.php";
 $router->include($final);
 
 include "project.php";
-$router->include($final);
-//project management
-$router->get("/FYPWise-web/milestoneform","Pages/project-management-mgt/milestone-form.php");
-$router->post("/FYPWise-web/milestoneform","Pages/project-management-mgt/milestone-form.php");
-$router->get("/FYPWise-web/milestonesubmission",page: "Pages/project-management-mgt/milestone-submission.php");
-$router->post("/FYPWise-web/milestonesubmission",page: "Pages/project-management-mgt/milestone-submission.php");
-$router->get("/FYPWise-web/projectplanapproval","Pages/project-management-mgt/project-plan-approval.php");
-$router->get("/FYPWise-web/projecttimelineplanning","Pages/project-management-mgt/project-timeline-planning.php");
-$router->get("/FYPWise-web/projectmanagement","Pages/project-management-mgt/projectmanagement.php");
-$router->get("/FYPWise-web/studentprojectassignment","Pages/project-management-mgt/student-project-assignment.php");
-$router->post("/FYPWise-web/studentprojectassignment","Pages/project-management-mgt/student-project-assignment.php");
-$router->get("/FYPWise-web/supervisorprojecttimeline","Pages/project-management-mgt/supervisor-project-timeline.php");
+$router->include($project);
 
 $router->dispatch();

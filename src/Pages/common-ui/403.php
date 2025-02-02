32ad4241605fd0313532c73b7b39f29aa365f08c
Firzan <firzanruzain@users.noreply.github.com>
2025-02-02 23:57:33 +0800

    bug on project.php

--- src/Pages/common-ui/403.php
+++ src/Pages/common-ui/403.php
@@ -1,41 +1,30 @@
-<!DOCTYPE html>
-<html>
+<?php
+use App\Models\Base;
+
+$base = new Base("Error 404");
+?>
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>FYPWise</title>
-    <link rel="icon" type="image/x-icon" href="./src/assets/main_logo.png">
-    <link rel="stylesheet" href="./src/css/common-ui.css">
-    <link rel="stylesheet" href="./src/css/home-page-style.css">
+    <link rel="stylesheet" href="/FYPWise-web/src/css/home-page-style.css?v=<?php echo time(); ?>">
 </head>
 
 <body>
     <!-- Header Section -->
     <header>
-        <button id="about-us"><a href="about-us"><img src="./src/assets/about us.png" alt="about us icon"
+        <button id="about-us"><a href="about-us"><img src="/FYPWise-web/src/assets/about us2.png" alt="about us icon"
                     class="aboutus-image"></a></button>
     </header>
 
+    
     <!-- FYPwise Logo -->
-    <img src="./src/assets/horizontal_logo.png" alt="FYPWise logo" id="FYPWise-logo">
+    <img src="/FYPWise-web/src/assets/horizontal_logo.png" alt="FYPWise logo" id="FYPWise-logo">
 
     <!-- Announcement -->
     <div class="announcement">
         <p>Error 403 - Forbidden</p>
     </div>
 
-    <footer>
-        <h3><a href="https://www.mmu.edu.my/">Multimedia University, Persiaran Multimedia, 63100 Cyberjaya, Selangor,
-                Malaysia</a></h3>
-        <div id="side">
-            <a class="link" href="http://www.mmu.edu.my/">MMU Website</a>
-            <a class="link" href="https://online.mmu.edu.my/">MMU Portal</a>
-            <a class="link" href="https://clic.mmu.edu.my/">CLiC</a>
-            <a class="link" href="https://servicedesk.mmu.edu.my/psp/crmprd/?cmd=login&languageCd=ENG&">Service Desk</a>
-        </div>
-        FYP Wise &copy; <em id="date"></em>Syabell Imran Aida Firzan
-    </footer>
+    <?php $base->renderFooter() ?>
 </body>
 
 </html>
